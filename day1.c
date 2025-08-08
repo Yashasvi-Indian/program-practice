ab3e36c7b79f05e6e9912846e8a3f07abc889a63
Yashasvi-Indian <yashasvisaxena89@gmail.com>
2025-08-08 10:18:13 +0530

    Add files via upload (#1)

---
+++ day1.c
@@ -0,0 +1,18 @@
+
+int main(){
+	int num;
+	char str[15];
+
+	scanf("%d", &num);              	                  // Reading input from STDIN
+	 for(int i=0;i<15;i++){
+		scanf("%s",str);
+	 }
+	//printf("");
+	//for(int i=0;i<=15;i++){
+		//scanf("%d",str[i]);}//
+	//now n*2
+	int b= num*2;
+      // Writing output to STDO//output
+	  printf("%d\n",b);
+	printf("%s",str);
+}
